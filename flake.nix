6146660097297315e04c74ce371460346962f67e
Eric Lehmann <katyl@katyl.info>
2025-11-27 08:19:07 -0500

    HW4 based updates

--- flake.nix
+++ flake.nix
@@ -3,66 +3,73 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-    ...
-  } @ inputs:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        fhsEnvironment = pkgs.buildFHSEnv {
-          name = "fhs-env";
-          targetPkgs = pkgs: [
-            pkgs.stdenv.cc.cc.lib
-            pkgs.libz
-          ];
-        };
-        devShell = pkgs.mkShell {
-          packages = [
-            pkgs.texlivePackages.latexmk
-            pkgs.python3
-            pkgs.python3Packages.pip
-            pkgs.python3Packages.virtualenv
-            pkgs.zstd
-            (pkgs.texliveSmall.withPackages (
-              ps:
-                with ps; [
-                  amsfonts
-                  appendix
-                  biblatex
-                  csquotes
-                  doublestroke
-                  enumitem
-                  esint
-                  framed
-                  pdfcol
-                  physics
-                  rsfs
-                  tikzfill
-                  tcolorbox
-                  titlesec
-                ]
-            ))
-          ];
-          inputsFrom = [fhsEnvironment];
-          shellHook = ''
-            if [ ! -d ".venv" ]; then
-              echo "Creating virtual environment..."
-              python -m venv .venv
-            fi
-            source .venv/bin/activate
-            pip install -r requirements.txt
-          '';
-        };
-      in {
-        devShells = {
-          default = devShell;
-        };
-      }
-    );
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    fhsEnvironment = {
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+        pkgs.stdenv.cc.cc.lib
+        pkgs.libz
+      ];
+    };
+    python = pkgs.mkShell {
+      packages = [
+        pkgs.python3
+        pkgs.python3Packages.pip
+        pkgs.imagemagick
+        pkgs.python3Packages.virtualenv
+        pkgs.zstd
+        pkgs.ghostscript
+      ];
+      shellHook = ''
+        if [ ! -d ".venv" ]; then
+          virtualenv .venv
+        fi
+        source .venv/bin/activate
+        if [ -f "requirements.txt" ]; then
+          pip install -r requirements.txt
+        fi
+      '';
+      env = fhsEnvironment;
+    };
+    latex = pkgs.mkShell {
+      packages = [
+        (pkgs.texliveSmall.withPackages (
+          ps:
+            with ps; [
+              amsfonts
+              appendix
+              biblatex
+              csquotes
+              doublestroke
+              enumitem
+              esint
+              framed
+              pdfcol
+              physics
+              rsfs
+              tikzfill
+              tcolorbox
+              titlesec
+            ]
+        ))
+      ];
+    };
+  in {
+    devShells.x86_64-linux = {
+      python = python;
+      latex = latex;
+      default = pkgs.mkShell {
+        inputsFrom = [
+          python
+          latex
+        ];
+        env = fhsEnvironment;
+      };
+    };
+  };
 }
