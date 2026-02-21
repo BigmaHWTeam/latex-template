354b7edb210e3ca0d1ed98ef134aec434d59143e
Eric Lehmann <katyl@katyl.info>
2026-02-21 14:28:00 -0500

    Updates for dmmu hw2

--- Makefile
+++ Makefile
@@ -25,6 +25,10 @@ ifeq ($(origin MAIN_FILE), undefined)
     endif
 endif
 
+# Output PDF name (without extension). Defaults to MAIN_FILE.
+# Edit this value directly, then run `make sync-ci` to propagate it to CI.
+BUILD_NAME ?= $(MAIN_FILE)
+
 # The LaTeX compiler
 LATEXMK = latexmk
 
@@ -32,14 +36,18 @@ LATEXMK = latexmk
 TEX_FILES = $(wildcard *.tex)
 
 # The default target
-all: $(MAIN_FILE).pdf
+all: build/$(BUILD_NAME).pdf
 
 # Rule to build the PDF
-$(MAIN_FILE).pdf: $(TEX_FILES)
-	$(LATEXMK) $(MAIN_FILE)
+build/$(BUILD_NAME).pdf: $(TEX_FILES)
+	$(LATEXMK) -jobname=$(BUILD_NAME) $(MAIN_FILE)
 
 # Clean up generated files
 clean:
-	rm -rf build *.aux *.bbl *.bcf *.blg *.dvi *.fdb_latexmk *.fls *.log *.out *.pdf *.ps *.run.xml
+	rm -rf build *.aux *.bbl *.bcf *.blg *.dvi *.fdb_latexmk *.fls *.log *.out *.ps *.run.xml
 
-.PHONY: all clean
+# Sync BUILD_NAME into the CI workflow
+sync-ci:
+	yq -i '.env.BUILD_NAME = "$(BUILD_NAME)"' .github/workflows/build.yml
+
+.PHONY: all clean sync-ci
--- flake.nix
+++ flake.nix
@@ -3,73 +3,85 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
 
   outputs = {
-    self,
+    flake-utils,
     nixpkgs,
-  }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    fhsEnvironment = {
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-        pkgs.stdenv.cc.cc.lib
-        pkgs.libz
-      ];
-    };
-    python = pkgs.mkShell {
-      packages = [
-        pkgs.python3
-        pkgs.python3Packages.pip
-        pkgs.imagemagick
-        pkgs.python3Packages.virtualenv
-        pkgs.zstd
-        pkgs.ghostscript
-      ];
-      shellHook = ''
-        if [ ! -d ".venv" ]; then
-          virtualenv .venv
-        fi
-        source .venv/bin/activate
-        if [ -f "requirements.txt" ]; then
-          pip install -r requirements.txt
-        fi
-      '';
-      env = fhsEnvironment;
-    };
-    latex = pkgs.mkShell {
-      packages = [
-        (pkgs.texliveSmall.withPackages (
-          ps:
-            with ps; [
-              amsfonts
-              appendix
-              biblatex
-              csquotes
-              doublestroke
-              enumitem
-              esint
-              framed
-              pdfcol
-              physics
-              rsfs
-              tikzfill
-              tcolorbox
-              titlesec
-            ]
-        ))
-      ];
-    };
-  in {
-    devShells.x86_64-linux = {
-      python = python;
-      latex = latex;
-      default = pkgs.mkShell {
-        inputsFrom = [
-          python
-          latex
-        ];
-        env = fhsEnvironment;
-      };
-    };
-  };
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        fhsEnvironment = {
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.libz
+          ];
+        };
+        python = pkgs.mkShell {
+          packages = [
+            pkgs.python3
+            pkgs.python3Packages.pip
+            pkgs.imagemagick
+            pkgs.python3Packages.virtualenv
+          ];
+          shellHook = ''
+            if [ ! -d ".venv" ]; then
+              virtualenv .venv
+            fi
+            source .venv/bin/activate
+            if [ -f "requirements.txt" ]; then
+              pip install -r requirements.txt
+            fi
+          '';
+          env = fhsEnvironment;
+        };
+        latex = pkgs.mkShell {
+          packages = [
+            pkgs.ghostscript
+            pkgs.yq-go
+            pkgs.zstd
+            pkgs.texlivePackages.latexmk
+            (pkgs.texliveSmall.withPackages (
+              ps:
+                with ps; [
+                  amsfonts
+                  appendix
+                  biblatex
+                  cleveref
+                  csquotes
+                  doublestroke
+                  enumitem
+                  esint
+                  framed
+                  pdfcol
+                  physics
+                  rsfs
+                  tikzfill
+                  tcolorbox
+                  titlesec
+                ]
+            ))
+          ];
+        };
+      in {
+        devShells = {
+          python = python;
+          latex = latex;
+          default = pkgs.mkShell {
+            inputsFrom = [
+              python
+              latex
+            ];
+            env = fhsEnvironment;
+          };
+        };
+      }
+    );
 }
